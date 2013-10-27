797c1620cc3748d18c9bd4127eeddebf45b883bb
D6C92FE5 <D6C92FE5@hotmail.com>
2013-10-27 22:22:29 +0800

    补上资源释放

--- Library_Management_System/Library_Management_System/_datastore_example.cpp
+++ Library_Management_System/Library_Management_System/_datastore_example.cpp
@@ -16,6 +16,7 @@ void InsertBook() {
     book->Total = 10;
     book->Remain = 10;
     Datastore::InsertOrUpdate(book);
+    delete book;
 }
 
 // 修改图书信息
@@ -23,6 +24,7 @@ void UpdateBook() {
     auto book = Datastore::Select<Datastore::Book>(0);
     book->Remain = 5;
     Datastore::InsertOrUpdate(book);
+    delete book;
 }
 
 // 删除图书信息
@@ -30,6 +32,16 @@ void DeleteBook() {
     Datastore::Delete<Datastore::Book>(0);
 }
 
+// 释放搜索结果
+template <typename T>
+void DestroyArray(T** array) {
+    auto temp = array;
+    while (*temp != NULL) {
+        delete *temp;
+        temp++;
+    }
+    delete array;
+}
 
 // 根据条件搜索图书
 bool SearchBookCondition(const Datastore::Book* book) {
@@ -37,6 +49,7 @@ bool SearchBookCondition(const Datastore::Book* book) {
 }
 void SearchBook() {
     auto books = Datastore::Select<Datastore::Book>(SearchBookCondition);
+    DestroyArray(books);
 }
 
 // 根据条件搜索图书 第2种写法
@@ -44,6 +57,7 @@ void SearchBook2() {
     auto books = Datastore::Select<Datastore::Book>([](const Datastore::Book* book) {
         return book->Remain > 0;
     });
+    DestroyArray(books);
 }
 
 // 修改用户
@@ -51,6 +65,7 @@ void UpdateUser() {
     auto user = Datastore::Select<Datastore::User>(0);
     strcpy(user->Password, "123456");
     Datastore::InsertOrUpdate(user);
+    delete user;
 }
 
 // 添加借阅记录
@@ -60,6 +75,7 @@ void InsertRecord() {
     record->UserIndex = 1;
     record->Datetime = time(NULL);
     Datastore::InsertOrUpdate(record);
+    delete record;
 }
 
 // 注意初始化
