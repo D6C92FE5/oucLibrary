d53df90b0d7d49fb6ca127d9ab9c4c950901c5f8
D6C92FE5 <D6C92FE5@hotmail.com>
2013-11-07 16:46:14 +0800

    替换制表符为空格

--- Library_Management_System/config.cpp
+++ Library_Management_System/config.cpp
@@ -6,27 +6,27 @@ namespace Config {
     const bool NORMAL_USER = false;
 
     const USER_ACCESS ADMIN_ACCESS = {
-		true,   // SEARCH_BOOK;		///检索图书
-		true,   // CHANGE_PASSWORD;	///修改密码
-		true,   // ADD_USER;	    ///添加用户
-		true,   // DELETE_USER;		///删除用户
-		true,   // RESET_PASSWORD;	///重置用户密码
-		true,   // BOLLOW_BOOK;		///借阅图书
-		true,   // RETURN_BOOK;		///归还图书
-		true,   // RENEW_BOOK;		///续借图书
-		true,   // BOLLOW_HISTORY;	///查看借阅历史
+        true,   // SEARCH_BOOK;     ///检索图书
+        true,   // CHANGE_PASSWORD; ///修改密码
+        true,   // ADD_USER;        ///添加用户
+        true,   // DELETE_USER;     ///删除用户
+        true,   // RESET_PASSWORD;  ///重置用户密码
+        true,   // BOLLOW_BOOK;     ///借阅图书
+        true,   // RETURN_BOOK;     ///归还图书
+        true,   // RENEW_BOOK;      ///续借图书
+        true,   // BOLLOW_HISTORY;  ///查看借阅历史
     };
 
     const USER_ACCESS NORMAL_ACCESS = {
-		true,   // SEARCH_BOOK;		///检索图书
-		true,   // CHANGE_PASSWORD;	///修改密码
-		false,  // ADD_USER;	    ///添加用户
-		false,  // DELETE_USER;		///删除用户
-		false,  // RESET_PASSWORD;	///重置用户密码
-		false,  // BOLLOW_BOOK;		///借阅图书
-		false,  // RETURN_BOOK;		///归还图书
-		false,  // RENEW_BOOK;		///续借图书
-		true,   // BOLLOW_HISTORY;	///查看借阅历史
+        true,   // SEARCH_BOOK;     ///检索图书
+        true,   // CHANGE_PASSWORD; ///修改密码
+        false,  // ADD_USER;        ///添加用户
+        false,  // DELETE_USER;     ///删除用户
+        false,  // RESET_PASSWORD;  ///重置用户密码
+        false,  // BOLLOW_BOOK;     ///借阅图书
+        false,  // RETURN_BOOK;     ///归还图书
+        false,  // RENEW_BOOK;      ///续借图书
+        true,   // BOLLOW_HISTORY;  ///查看借阅历史
     };
 
     const int MAX_BOLLOW_TIME = 30;
--- Library_Management_System/config.h
+++ Library_Management_System/config.h
@@ -1,28 +1,28 @@
 #pragma once
 namespace Config {
 
-	//用户类型
-	extern const bool ADMIN_USER;
-	extern const bool NORMAL_USER;
+    //用户类型
+    extern const bool ADMIN_USER;
+    extern const bool NORMAL_USER;
 
-	//用户权限
-	typedef struct {
-		bool SEARCH_BOOK;		///检索图书
-		bool CHANGE_PASSWORD;	///修改密码
-		bool ADD_USER;			///添加用户
-		bool DELETE_USER;		///删除用户
-		bool RESET_PASSWORD;	///重置用户密码
-		bool BOLLOW_BOOK;		///借阅图书
-		bool RETURN_BOOK;		///归还图书
-		bool RENEW_BOOK;		///续借图书
-		bool BOLLOW_HISTORY;	///查看借阅历史
-	} USER_ACCESS;
+    //用户权限
+    typedef struct {
+        bool SEARCH_BOOK;       ///检索图书
+        bool CHANGE_PASSWORD;   ///修改密码
+        bool ADD_USER;          ///添加用户
+        bool DELETE_USER;       ///删除用户
+        bool RESET_PASSWORD;    ///重置用户密码
+        bool BOLLOW_BOOK;       ///借阅图书
+        bool RETURN_BOOK;       ///归还图书
+        bool RENEW_BOOK;        ///续借图书
+        bool BOLLOW_HISTORY;    ///查看借阅历史
+    } USER_ACCESS;
     extern const USER_ACCESS ADMIN_ACCESS;
     extern const USER_ACCESS NORMAL_ACCESS;
 
-	//借阅期限
-	extern const int MAX_BOLLOW_TIME;
+    //借阅期限
+    extern const int MAX_BOLLOW_TIME;
 
-	//借阅上限
-	extern const int MAX_BOLLOW_NUM;
+    //借阅上限
+    extern const int MAX_BOLLOW_NUM;
 }
