3ff934015547843a0b358f00cc182335499f1e1b
WinUP <winupsoftware@gmail.com>
2013-11-13 16:51:02 +0800

    忘记包括main.cpp了...

---
+++ Library_Management_System/main.cpp
@@ -0,0 +1,29 @@
+#include "datastore.h"
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+//插入示例图书的代码
+int __main ()
+{
+	Datastore::Init();
+	ifstream infile("书目信息.txt");
+	string temp;
+	while (getline(infile, temp))
+	{
+		auto TmpBook = Datastore::Create<Datastore::Book>();
+		strcpy(TmpBook->Name, temp.c_str());
+		getline(infile, temp);
+		strcpy(TmpBook->Author, temp.c_str());
+		getline(infile, temp);
+		strcpy(TmpBook->Publisher, temp.c_str());
+		getline(infile, temp);
+		strcpy(TmpBook->Isbn, temp.c_str());
+		TmpBook->Total = rand() / 500;
+		TmpBook->Remain = TmpBook->Total;
+		Datastore::InsertOrUpdate(TmpBook);
+		delete TmpBook;
+	}
+	return 0;
+}
