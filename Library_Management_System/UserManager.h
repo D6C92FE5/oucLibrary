c05194d28b876fb26c672cb79fb2217d4fceba4b
蒋震霆 <JiangZhT@outlook.com>
2013-11-22 19:27:34 +0800

    修改

--- Library_Management_System/UserManager.h
+++ Library_Management_System/UserManager.h
@@ -16,18 +16,18 @@ namespace UserManager{
 	//用户升级
 	bool UpLevel(string Name, string Password);
 	//用户修改密码
-	void UpdataOnesPassword(string Password);
+	bool UpdataOnesPassword(string Password);
 	//用户修改INFO
-	void UpdataOnesInfo(string Info);
+	bool UpdataOnesInfo(string Info);
 	//管理员所有
 	//创建新用户
-	void InsertUser(string Name, string Password);
+	bool InsertUser(string Name, string Password);
 	//选择用户
 	Datastore::User * SelectUser(string Name);
 	//删除用户
-	void DeleteUser(string Name);
+	bool DeleteUser(string Name);
 	//更新用户密码
-	void UpdataUserPassword(string Name, string Password = "");
+	bool UpdataUserPassword(string Name, string Password);
 	//更新用户INFO
-	void UpdataUserInfo(string Name, string Info = "");
+	bool UpdataUserInfo(string Name, string Info = "");
 }
